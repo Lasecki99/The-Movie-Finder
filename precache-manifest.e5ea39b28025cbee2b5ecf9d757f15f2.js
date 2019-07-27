432da9f497f4309c26d42b42f4c785843069231c
Lasecki99 <marcin.lasecki99@gmail.com>
2019-07-27 17:21:36 +0200

    Updates

---
+++ precache-manifest.e5ea39b28025cbee2b5ecf9d757f15f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b2953d0ae24cf60c386c70e3d2ec9b",
+    "url": "/The-Movie-Finder/index.html"
+  },
+  {
+    "revision": "fca352c2c2a449f67a28",
+    "url": "/The-Movie-Finder/static/css/2.4872d0fd.chunk.css"
+  },
+  {
+    "revision": "36686e471eb0c19b5c0d",
+    "url": "/The-Movie-Finder/static/css/main.1bc8f539.chunk.css"
+  },
+  {
+    "revision": "fca352c2c2a449f67a28",
+    "url": "/The-Movie-Finder/static/js/2.cd81cc50.chunk.js"
+  },
+  {
+    "revision": "36686e471eb0c19b5c0d",
+    "url": "/The-Movie-Finder/static/js/main.ed6938bb.chunk.js"
+  },
+  {
+    "revision": "8badf05a6cd756a8c4db",
+    "url": "/The-Movie-Finder/static/js/runtime~main.ced94bfe.js"
+  },
+  {
+    "revision": "b03c571c6960ea76bafaf3879d475b87",
+    "url": "/The-Movie-Finder/static/media/react.b03c571c.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/The-Movie-Finder/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/The-Movie-Finder/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/The-Movie-Finder/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/The-Movie-Finder/static/media/slick.f97e3bbf.svg"
+  }
+]);
