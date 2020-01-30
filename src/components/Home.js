2ebfe8c91ddaafc018a933bd2a53dc71846b644a
Marcin Lasecki <marcin.lasecki@pearson.com>
2020-01-30 14:37:11 +0100

    replace Home class component to functional component and update project dependencies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/Home.css'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-class Home extends Component {
+const Home = (props) => {
 
-    state = {}
-
-    render() {
-        const movie = this.props.movies.map(item => {
-            if (item.backdrop_path) {
-                return (
-                    <div key={item.id}>
-                        <img src={`https://image.tmdb.org/t/p/original/` + item.backdrop_path} alt="" />
-                        <h4>{item.title}</h4>
-                        <h5 onClick={() => this.props.id(item.id)}>See details</h5>
-                    </div>
-                )
-            } else return null;
-        });
-        return (
-            <div className='movie'>
-                <ul className="list">
-                    <Carousel
-                        showIndicators={false}
-                        showThumbs={false}
-                        showStatus={false}
-                        infiniteLoop={true}
-                        autoPlay={true}>{movie}</Carousel>
-                </ul>
-            </div >
-        );
-    }
+    const movie = props.movies.map(item => {
+        if (item.backdrop_path) {
+            return (
+                <div key={item.id}>
+                    <img src={`https://image.tmdb.org/t/p/original/` + item.backdrop_path} alt={item.title} />
+                    <h4>{item.title}</h4>
+                    <h5 onClick={() => props.id(item.id)}>See details</h5>
+                </div>
+            )
+        } else return null;
+    });
+    return (
+        <div className='movie'>
+            <ul className="list">
+                <Carousel
+                    showIndicators={false}
+                    showThumbs={false}
+                    showStatus={false}
+                    infiniteLoop={true}
+                    autoPlay={true}>{movie}
+                </Carousel>
+            </ul>
+        </div >
+    );
 }
 
 export default Home;
